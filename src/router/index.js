d2082c291f716e4f981ccde7f7a474842142541b
zhangfei <zhangfei@wisecotech.com>
2020-12-08 10:51:55 +0800

    feat：login

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,12 @@ export default new Router({
   mode: 'history',
   routes: [
     {
+      path: '/login',
+      meta: { title: '登录', noCache: true },
+      component: resolve => require(['@/views/login/index'], resolve),
+      hidden: true
+    },
+    {
       path: '/',
       name: 'Index',
       component: resolve => require(['@/views/main/index'], resolve)
---
+++ src/views/login/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="login" :style="`background-image:url('${back}')`">
+    <el-card class="login-form">
+
+    </el-card>
+  </div>
+</template>
+
+<script>
+import { getDepts } from '@/api/login'
+import Background from '@/assets/images/back.jpg'
+export default {
+  data() {
+    return {
+      back: Background
+    }
+  },
+  computed: {
+    
+  },
+  created() {
+    
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .login {
+    width: 100%;
+    height: 100vh;
+    background-size:100% 100%;
+    background-repeat:no-repeat;
+    background-attachment: fixed;
+  }
+  .login-form {
+    width: 400px;
+    height: 350px;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
